49ab3c1e236961909df332e3ffe6a288dcab9377
John_Ballen <jhonn.ballen@gmail.com>
2021-10-26 16:26:36 -0500

    comentario sobre here-document archivo

--- here-document.php
+++ here-document.php
@@ -2,7 +2,7 @@
 
   //Para hacer un here document, se debe poner el cuerpo del texto deseado entre las entiquetas _END
   $author = "John F. Ballen";
-  echo <<<_END
+  echo <<<_END //Se puede usar cualquier etiqueta diferente a _END, ejemplo _SECTION1, _OUTPUT, ETC... 
   Debugging is twice as hard as writing teh code in the first place.
   There fore, if you write the code as cleverly as possible,
   you are by definition, nos smart enough to debug it.
